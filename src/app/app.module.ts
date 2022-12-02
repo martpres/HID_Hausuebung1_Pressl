90a007681141818957359d3ccb8ae3daa5aec1b8
Martin Pressl <99990993+martpres@users.noreply.github.com>
2022-12-02 23:04:52 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddSensorsDataComponent } from './dashboard/add-sensors-data/add-sensors-data.component';
+import { ButtonComponent } from './button/button.component';
+import { HeaderComponent } from './header/header.component';
+import { SensorsDataComponent } from './dashboard/sensors-data/sensors-data.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    AddSensorsDataComponent,
+    ButtonComponent,
+    HeaderComponent,
+    SensorsDataComponent,
+    AboutPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
